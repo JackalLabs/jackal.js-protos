4f0c3a3133141db825a6e1b58c542c257ba55bc4
Erin Rivas <erin@atomicbear.com>
2023-05-15 13:58:07 -0500

    splitting out query class

--- src/index.ts
+++ src/index.ts
@@ -1,220 +1,29 @@
-import { OfflineSigner } from '@cosmjs/proto-signing'
-import { genBroadcaster, IGenBroadcaster, TMasterBroadcaster } from '@/customBroadcast'
+export { ProtoBuilder, QueryBuilder } from '@/builders'
+export { default as IProtoBuilder } from '@/interfaces/classes/IProtoBuilder'
+export { default as IQueryBuilder } from '@/interfaces/classes/IQueryBuilder'
 
-import IQueryABCI from '@/interfaces/classes/IQueryABCI'
-import IProtoBuilder from '@/interfaces/classes/IProtoBuilder'
-import IQueryFileTree from '@/interfaces/classes/IQueryFileTree'
-import IQueryJklMint from '@/interfaces/classes/IQueryJklMint'
-import IQueryNotifications from '@/interfaces/classes/IQueryNotifications'
-import IQueryOracle from '@/interfaces/classes/IQueryOracle'
-import IQueryRns from '@/interfaces/classes/IQueryRns'
-import IQueryStorage from '@/interfaces/classes/IQueryStorage'
-import IQueryBank from '@/interfaces/classes/IQueryBank'
-import IQueryDistribution from '@/interfaces/classes/IQueryDistribution'
-import IQueryGov from '@/interfaces/classes/IQueryGov'
-import IQuerySlashing from '@/interfaces/classes/IQuerySlashing'
-import IQueryStaking from '@/interfaces/classes/IQueryStaking'
+export { IGenBroadcaster, TMasterBroadcaster } from '@/customBroadcast'
 
-import {
-  GrpcWebImpl,
-  IABCIGrpc,
-  IFileTreeGrpc,
-  IJklMintGrpc,
-  INotificationsGrpc,
-  IOracleGrpc,
-  IRnsGrpc,
-  IStorageGrpc,
-  IBankGrpc,
-  IDistributionGrpc,
-  IGovGrpc,
-  ISlashingGrpc,
-  IStakingGrpc,
-  TGrpc
-} from '@/interfaces/IGrpcWebImpl'
+export { default as IQueryABCI } from '@/interfaces/classes/IQueryABCI'
+export { default as IQueryFileTree } from '@/interfaces/classes/IQueryFileTree'
+export { default as IQueryJklMint } from '@/interfaces/classes/IQueryJklMint'
+export { default as IQueryNotifications } from '@/interfaces/classes/IQueryNotifications'
+export { default as IQueryOracle } from '@/interfaces/classes/IQueryOracle'
+export { default as IQueryRns } from '@/interfaces/classes/IQueryRns'
+export { default as IQueryStorage } from '@/interfaces/classes/IQueryStorage'
+export { default as IQueryBank } from '@/interfaces/classes/IQueryBank'
+export { default as IQueryDistribution } from '@/interfaces/classes/IQueryDistribution'
+export { default as IQueryGov } from '@/interfaces/classes/IQueryGov'
+export { default as IQuerySlashing } from '@/interfaces/classes/IQuerySlashing'
+export { default as IQueryStaking } from '@/interfaces/classes/IQueryStaking'
 
-import QueryABCI from '@/snackages/query/static/abci'
-import QueryFileTree from '@/snackages/query/custom/fileTree'
-import QueryJklMint from '@/snackages/query/custom/jklMint'
-import QueryNotifications from '@/snackages/query/custom/notifications'
-import QueryOracle from '@/snackages/query/custom/oracle'
-import QueryRns from '@/snackages/query/custom/rns'
-import QueryStorage from '@/snackages/query/custom/storage'
-import QueryBank from '@/snackages/query/static/bank'
-import QueryDistribution from '@/snackages/query/static/distribution'
-import QueryGov from '@/snackages/query/static/gov'
-import QuerySlashing from '@/snackages/query/static/slashing'
-import QueryStaking from '@/snackages/query/static/staking'
-
-import { ITxFileTree, TxFileTree } from '@/snackages/tx/custom/fileTree'
-import { ITxNotifications, TxNotifications } from '@/snackages/tx/custom/notifications'
-import { ITxOracle, TxOracle } from '@/snackages/tx/custom/oracle'
-import { ITxRns, TxRns } from '@/snackages/tx/custom/rns'
-import { ITxStorage, TxStorage } from '@/snackages/tx/custom/storage'
-import { ITxBank, TxBank } from '@/snackages/tx/static/bank'
-import { ITxDistribution, TxDistribution } from '@/snackages/tx/static/distribution'
-import { ITxGov, TxGov } from '@/snackages/tx/static/gov'
-import { ITxSlashing, TxSlashing } from '@/snackages/tx/static/slashing'
-import { ITxStaking, TxStaking } from '@/snackages/tx/static/staking'
-import IAllQuery from '@/interfaces/IAllQuery'
-import IAllTx from '@/interfaces/IAllTx'
-
-export default class ProtoBuilder implements IProtoBuilder {
-  private readonly signer: OfflineSigner
-  private readonly txUrl: string
-  private readonly GRpc: TGrpc
-
-  constructor (signer: OfflineSigner, txUrl?: string, queryUrl?: string) {
-    const qUrl = queryUrl || 'http://localhost:9091'
-    this.signer = signer
-    this.txUrl = txUrl || 'http://localhost:26657'
-    this.GRpc = new GrpcWebImpl(qUrl, {})
-  }
-
-  /** General */
-  makeMasterBroadcaster (): Promise<IGenBroadcaster> {
-    return genBroadcaster(this.signer, { addr: this.txUrl })
-      .catch(err => {
-        throw err
-      })
-  }
-  makeAllQuery (): IAllQuery {
-    return {
-      /** Custom */
-      fileTree: this.makeFileTreeQuery(),
-      jklMint: this.makeJklMintQuery(),
-      notifications: this.makeNotificationsQuery(),
-      oracle: this.makeOracleQuery(),
-      rns: this.makeRnsQuery(),
-      storage: this.makeStorageQuery(),
-      /** Static */
-      abci: this.makeABCIQuery(),
-      bank: this.makeBankQuery(),
-      distribution: this.makeDistributionQuery(),
-      gov: this.makeGovQuery(),
-      slashing: this.makeSlashingQuery(),
-      staking: this.makeStakingQuery()
-    }
-  }
-  makeAllTx (): IAllTx {
-    return {
-      /** Custom */
-      fileTree: this.makeFileTreeTx(),
-      jklMint: null,
-      notifications: this.makeNotificationsTx(),
-      oracle: this.makeOracleTx(),
-      rns: this.makeRnsTx(),
-      storage: this.makeStorageTx(),
-      /** Static */
-      bank: this.makeBankTx(),
-      distribution: this.makeDistributionTx(),
-      gov: this.makeGovTx(),
-      slashing: this.makeSlashingTx(),
-      staking: this.makeStakingTx()
-    }
-  }
-
-  /** Custom */
-  makeFileTreeQuery (): IQueryFileTree {
-    return new QueryFileTree(this.GRpc as IFileTreeGrpc)
-  }
-  makeFileTreeTx (): ITxFileTree {
-    return new TxFileTree()
-  }
-  makeJklMintQuery (): IQueryJklMint {
-    return new QueryJklMint(this.GRpc as IJklMintGrpc)
-  }
-  makeNotificationsQuery (): IQueryNotifications {
-    return new QueryNotifications(this.GRpc as INotificationsGrpc)
-  }
-  makeNotificationsTx (): ITxNotifications {
-    return new TxNotifications()
-  }
-  makeOracleQuery (): IQueryOracle {
-    return new QueryOracle(this.GRpc as IOracleGrpc)
-  }
-  makeOracleTx (): ITxOracle {
-    return new TxOracle()
-  }
-  makeRnsQuery (): IQueryRns {
-    return new QueryRns(this.GRpc as IRnsGrpc)
-  }
-  makeRnsTx (): ITxRns {
-    return new TxRns()
-  }
-  makeStorageQuery (): IQueryStorage {
-    return new QueryStorage(this.GRpc as IStorageGrpc)
-  }
-  makeStorageTx (): ITxStorage {
-    return new TxStorage()
-  }
-
-  /** Static */
-  makeABCIQuery (): IQueryABCI {
-    return new QueryABCI(this.GRpc as IABCIGrpc)
-  }
-  makeBankQuery (): IQueryBank {
-    return new QueryBank(this.GRpc as IBankGrpc)
-  }
-  makeBankTx (): ITxBank {
-    return new TxBank()
-  }
-  makeDistributionQuery (): IQueryDistribution {
-    return new QueryDistribution(this.GRpc as IDistributionGrpc)
-  }
-  makeDistributionTx (): ITxDistribution {
-    return new TxDistribution()
-  }
-  makeGovQuery (): IQueryGov {
-    return new QueryGov(this.GRpc as IGovGrpc)
-  }
-  makeGovTx (): ITxGov {
-    return new TxGov()
-  }
-  makeSlashingQuery (): IQuerySlashing {
-    return new QuerySlashing(this.GRpc as ISlashingGrpc)
-  }
-  makeSlashingTx (): ITxSlashing {
-    return new TxSlashing()
-  }
-  makeStakingQuery (): IQueryStaking {
-    return new QueryStaking(this.GRpc as IStakingGrpc)
-  }
-  makeStakingTx (): ITxStaking {
-    return new TxStaking()
-  }
-}
-
-export {
-  /** General */
-  IAllQuery,
-  IAllTx,
-  IGenBroadcaster,
-  IProtoBuilder,
-  TMasterBroadcaster,
-
-  /** Custom */
-  IQueryFileTree,
-  ITxFileTree,
-  IQueryJklMint,
-  IQueryNotifications,
-  ITxNotifications,
-  IQueryOracle,
-  ITxOracle,
-  IQueryRns,
-  ITxRns,
-  IQueryStorage,
-  ITxStorage,
-
-  /** Static */
-  IQueryABCI,
-  IQueryBank,
-  ITxBank,
-  IQueryDistribution,
-  ITxDistribution,
-  IQueryGov,
-  ITxGov,
-  IQuerySlashing,
-  ITxSlashing,
-  IQueryStaking,
-  ITxStaking
-}
+export { ITxFileTree } from '@/snackages/tx/custom/fileTree'
+export { ITxNotifications } from '@/snackages/tx/custom/notifications'
+export { ITxOracle } from '@/snackages/tx/custom/oracle'
+export { ITxRns } from '@/snackages/tx/custom/rns'
+export { ITxStorage } from '@/snackages/tx/custom/storage'
+export { ITxBank } from '@/snackages/tx/static/bank'
+export { ITxDistribution } from '@/snackages/tx/static/distribution'
+export { ITxGov } from '@/snackages/tx/static/gov'
+export { ITxSlashing } from '@/snackages/tx/static/slashing'
+export { ITxStaking } from '@/snackages/tx/static/staking'
