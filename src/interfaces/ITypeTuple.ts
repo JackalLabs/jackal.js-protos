43325fcfa868094c2051eea539670121cadf54a0
Erin Rivas <erin@atomicbear.com>
2023-11-16 13:10:02 -0600

    out with the old
    project configs

--- src/interfaces/ITypeTuple.ts
+++
@@ -1,3 +0,0 @@
-export default interface ITypeTuple {
-  [key: string]: [string, any]
-}
--- src/types/TSuccessNoUndefined.ts
+++
@@ -1,6 +0,0 @@
-import NonNullable from '@/types/TNoUndefined'
-import SuccessIncluded from '@/types/TSuccessIncluded'
-
-type SuccessNoUndefined<T> = NonNullable<SuccessIncluded<T>>
-
-export default SuccessNoUndefined
